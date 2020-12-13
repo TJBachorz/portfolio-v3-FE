4a6ecd8d5a5f0107de698d3c871862fff6605f0a
TJBachorz <TJBachorz@gmail.com>
2020-12-13 13:45:14 -0700

    initial commit, working arwes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider, createTheme, withStyles, Arwes, Puffs } from 'arwes';
 
 function App() {
+
+  const myTheme = {
+    color: {
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={createTheme(myTheme)}>
+      <Arwes>
+        <Puffs>
+          <div className="puffs" style={{ width: '100%', height: '100vh' }}>
+            <div className="main-container">
+              <h1>Thomas Bachorz: Full-Stack Software Developer</h1>
+            </div>
+          </div>
+        </Puffs>
+      </Arwes>
+    </ThemeProvider>
   );
 }
 
