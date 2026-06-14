72697bad850bf733fde8963253a21130d1a90561
TJ <tjbachorz@gmail.com>
2026-06-14 12:45:33 -0600

    feat: add Footer and complete App layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Projects />
         <Contact />
       </main>
+      <Footer />
     </>
   )
 }
